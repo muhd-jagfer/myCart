f7de6dd42e6f8f79c003b2cdd4f71dea37346c96
muhd-jagfer <muhdjagfer@gmail.com>
2026-09-20 17:35:22 +0530

    implementing localstorage and delte in cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -97,8 +97,11 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
 document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
-    const productId = link.dataset.productId;
+    const productId = link.dataset.productLink;
     removeFromCart(productId);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
 
