3ebdc7b458bbb0e2cf4e07e01e67446822f0caaf
muhd-jagfer <muhdjagfer@gmail.com>
2026-09-24 21:09:46 +0530

    implementing: test jasmine

--- tests-jasmine/lib/moneyTest.js
+++
@@ -1,7 +0,0 @@
-import {formatCurrency} from '../scripts/utils/money.js';
-
-describe('test suite: formatCurrency', () => {
-  it('converts cents into dollars', () => {
-    expect(formatCurrency(2095).toEqual('20.95'));
-  });
-});      
